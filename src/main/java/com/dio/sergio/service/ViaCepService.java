dc30570f2be31523d3c1b94a03c82bef3cd92703
Sérgio Melo <sergio.dimello22@gmail.com>
2024-11-17 20:27:29 -0300

    Initial commit

---
+++ src/main/java/com/dio/sergio/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package com.dio.sergio.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.dio.sergio.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+	@GetMapping("/{cep}/json/")
+	Endereco consultarCep(@PathVariable("cep") String cep);
+}
